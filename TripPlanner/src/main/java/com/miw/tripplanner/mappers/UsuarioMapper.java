4a6b6d06ba9d6fdf2c85f01682b05f1bf952ae87
Rodrigo de la Calle <rodricalle@usal.es>
2024-06-14 21:36:00 +0200

    Añadida la funcionalidad de Usuarios iwvg-hugo/TFM#6

---
+++ TripPlanner/src/main/java/com/miw/tripplanner/mappers/UsuarioMapper.java
@@ -0,0 +1,17 @@
+package com.miw.tripplanner.mappers;
+
+import com.miw.tripplanner.dtos.*;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface UsuarioMapper {
+    
+    List<UsuarioDto> getAllUsuarios();
+    UsuarioDto getUsuario(Integer id);
+    Integer createUsuario(UsuarioDto usuarioDto);
+    void updateUsuario(Integer id, UsuarioDto usuarioDto);
+    void deleteUsuario(Integer id);
+
+}
