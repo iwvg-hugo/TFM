c2abceafb2ddc6991e84de5f4eca787737cf9964
Hugo <hvd28@hotmail.es>
2024-06-15 16:44:39 +0200

    [prueba]

---
+++ TripPlanner/src/test/java/com/miw/tripplanner/dtos/HorarioDtoTest.java
@@ -0,0 +1,14 @@
+package com.miw.tripplanner.dtos;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+
+class HorarioDtoTest {
+
+    @Test
+    void testEqualsAndHashCode() {
+        EqualsVerifier.simple().forClass(HorarioDto.class)
+                .withRedefinedSuperclass() // Permite que la clase no sea final
+                .verify();
+    }
+}
