f33b1ddf832c19ac9aad625d7a36eb1f724d5f13
Rodrigo de la Calle <rodricalle@usal.es>
2024-06-17 20:35:10 +0200

    Arreglados los tests de Usuario iwvg-hugo/TFM#6

--- TripPlanner/src/test/resources/db/changelog/data/test/0005-data-usuarios.sql
+++ TripPlanner/src/test/resources/db/changelog/data/test/0005-data-usuarios.sql
@@ -1,4 +1,4 @@
 --changeset tripPlanner:0004-data-usuarios
 
-insert into usuarios(id, name, password, email)
-values (9999, 'admin', 'admin', 'correo@mail.com');
+insert into usuarios(id, fullname, password, email, created_at, updated_at)
+values (9999, 'admin', 'admin', 'correo@mail.com', default, default);
